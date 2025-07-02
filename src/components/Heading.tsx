b5dfda2ec63c25caae3aa1af4121c533abdf8a06
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-07-02 15:29:21 -0600

    Add lucide-icons library into the proyect

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,3 +1,4 @@
+import { Plus } from "lucide-react";
 import { Link } from "react-router";
 
 export function Heading() {
@@ -5,14 +6,16 @@ export function Heading() {
     <div className="flex items-center mb-10">
         <h5 className="text-gray-100 font-bold text-2xl">
             <Link to='/'>
-            
                 Task App
             </Link>
         </h5>
         <div className="flex-grow text-right px-4 py-2 m-2">
-            <button className="bg-green-400 hover:bg-green-500 text-while font-semibold py-2 px-4 rounded inline-flex items-center">
-                Add Employee
-            </button>
+                <Link to="/add">        
+                    <button className="bg-green-400 hover:bg-green-500 text-while font-semibold py-2 px-4 rounded inline-flex items-center hover:cursor-pointer">
+                        <Plus />
+                        Add Employee
+                    </button>
+                </Link>
         </div>
     </div>
   )
