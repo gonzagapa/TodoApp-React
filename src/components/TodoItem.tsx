c8641d7d68c7ace6b2e22f4ab9fe3b8c30bc7811
Gonzalo Perez <gonzalo.anuar13@outlook.com>
2025-07-08 20:43:09 -0600

    Add Delete functionality in item button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,10 +1,12 @@
-import type { Todo } from ".."
+import type { Todo } from "../data"
+import { useTodoContext } from "../hooks/useTodoContext"
 
 interface Props {
     todo:Todo
 }
 
 export default function TodoItem({todo}:Props) {
+        const {dispatch} = useTodoContext();
   return (
     <div className="p-2.5 rounded-xl bg-gray-950 flex justify-between gap-2 items-center w-sm">
         <div className="flex-1/3">
@@ -14,7 +16,7 @@ export default function TodoItem({todo}:Props) {
         </div>
         <div className="flex flex-col gap-2">
             <button className="bg-green-600 hover:bg-green-500 text-while font-semibold py-2 px-4 cursor-pointer">Edit</button>
-            <button className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 cursor-pointer">Delete</button>
+            <button className="bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 cursor-pointer" onClick={()=> {dispatch({type:"[TODO] delete", payload:todo})}}>Delete</button>
         </div>
     </div>
   )
